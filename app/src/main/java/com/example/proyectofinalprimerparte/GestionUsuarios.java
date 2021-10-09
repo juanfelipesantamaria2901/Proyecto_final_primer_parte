634ba5c06735d015311333753efd7f94655ca554
Jaime Andres <jaime.artunduaga@uao.edu.co>
2021-10-08 22:00:11 -0500

    InterfazGestiónUsuarios

---
+++ app/src/main/java/com/example/proyectofinalprimerparte/GestionUsuarios.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinalprimerparte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_usuarios);
+    }
+}
